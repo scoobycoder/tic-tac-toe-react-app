9cdde93a457abf5ef9265503b23be1f621aa4ebb
na5j2t4 <nathan_halterman@gap.com>
2018-08-23 09:41:17 -0400

    fixed tests

--- src/Cell.js
+++ src/Cell.js
@@ -6,18 +6,14 @@ class Cell extends React.Component {
         super(props)
         this.state = { mark: ""}
 
-        // this.markIt = this.markIt.bind(this)
+        this.markIt = this.markIt.bind(this)
     }
 
     render() {
         return <div className="cell" onClick = {this.markIt}>{this.state.mark}</div>
     }
 
-    // markIt() {
-    //     this.setState({ mark: "X"})
-    // }
-
-    markIt = () => {
+    markIt() {
         this.setState({ mark: "X"})
     }
 }
